a00e626169a12dbcfa4646ff04f49336b89a4944
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2021-12-24 20:48:28 +0800

    [comment]:fix comment

--- Plugins/SimpleThread/Source/SimpleThread/Public/Core/SimpleThreadType.h
+++ Plugins/SimpleThread/Source/SimpleThread/Public/Core/SimpleThreadType.h
@@ -1,6 +1,9 @@
-﻿/*
-此文件用以定义那些杂项的变量、类、函数、函数指针
-*/
+﻿/************************************************************************/
+/* 此头文件主要定义那些特殊的线程类型 
+* 如单播代理、 LAMBDA、
+   杂项的变量、类、函数、函数指针、结构体*/
+/************************************************************************/
+
 #pragma once
 #include "CoreMinimal.h"
 #include "Templates/SharedPointer.h"
@@ -8,8 +11,10 @@
 /* 用以查询并控制线程状态的 线程句柄*/
 struct SIMPLETHREAD_API FSimpleThreadHandle : public TSharedFromThis<FSimpleThreadHandle>
 {
+	/* 构造器用以生成随机线程ID*/
 	FSimpleThreadHandle();
 
+	/* 重载操作符用以检索是否匹配外部句柄*/
 	bool operator==(const FSimpleThreadHandle& simpleThreadHandle)
 	{
 		return
@@ -19,6 +24,7 @@ struct SIMPLETHREAD_API FSimpleThreadHandle : public TSharedFromThis<FSimpleThre
 			this->GUIDTypeD == simpleThreadHandle.GUIDTypeD;
 	}
 protected:
+	/* 通过这4个不同的ID来寻找我们的线程*/
 	uint64 GUIDTypeA;
 	uint64 GUIDTypeB;
 	uint64 GUIDTypeC;
@@ -26,5 +32,5 @@ protected:
 
 };
 
-typedef TWeakPtr<FSimpleThreadHandle> FWeakThreadHandle; // 注意!!这里是弱指针 线程句柄!!把这个线程句柄同时也声明为1根弱指针
+typedef TWeakPtr<FSimpleThreadHandle> FWeakThreadHandle; // 弱指针包装的线程句柄(携带线程ID); 注意!!这里是弱指针 线程句柄!!把这个线程句柄同时也声明为1根弱指针
 typedef TFunction<void()> FThradLambda;// 一根函数指针, 用于线程的lambda
--- Plugins/SimpleThread/Source/SimpleThread/Public/Interface/ProxyInterface.h
+++ Plugins/SimpleThread/Source/SimpleThread/Public/Interface/ProxyInterface.h
@@ -3,14 +3,14 @@
 #include "Templates/SharedPointer.h"
 #include "Core/SimpleThreadType.h"
 
-/*
-通过此接口来实现 线程里应该具备的一些功能
-*/
-
+/**
+ * 借助此接口来实现 真正线程里应该具备的一些功能
+ * TSharedFromThis<>类,携带弱指针
+ */
 class SIMPLETHREAD_API IThreadProxy : public TSharedFromThis<IThreadProxy>
 {
 public:
-	IThreadProxy();
+	IThreadProxy();// 
 	virtual ~IThreadProxy();
 	virtual void SuspendThread() = 0;// 一个是挂起
 	virtual void WakeupThread() = 0;// 一个是唤醒
@@ -18,10 +18,10 @@ public:
 	virtual void CreateSafeThread() = 0;// 强制子类重写此函数 来创建线程实例
 	FORCEINLINE FSimpleDelegate& GetThreadDelegate() { return ThreadDelegate; }
 	FORCEINLINE FThradLambda& GetThreadLambda() { return ThreadLambda; }
-	FORCEINLINE FWeakThreadHandle GetThreadHandle() { return (SimpleThreadHandle); }// 注意此接口返回的是弱指针,弱指针声明在Type.h里
+	FORCEINLINE FWeakThreadHandle GetThreadHandle() { return (SimpleThreadHandle); }// 拿句柄的弱引用版,注意此接口返回的是弱指针,弱指针声明在Type.h里
 
 protected:
-	FSimpleDelegate ThreadDelegate;// UE4内建单播代理的一种
+	FSimpleDelegate ThreadDelegate;// UE4内建单播代理的一种,UE引擎自己的单播代理
 	FThradLambda ThreadLambda;// 一根函数指针,线程Lambda
 private:
 	TSharedPtr<FSimpleThreadHandle> SimpleThreadHandle;// 线程句柄,这里是强引用!!!
