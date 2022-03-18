f1175f0252cb5a4732766f0717736f0fd846f668
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-03-18 23:06:51 +0800

    Squashed commits: [feat]:封装协程对象.
    /// 协程请求.
    struct FCoroutinesRequest
    
    /// 协程接口.
    class ICoroutinesObject : public TSharedFromThis<ICoroutinesObject>
    
    /// 协程对象.
    class FCoroutinesObject : public ICoroutinesObject

---
+++ Plugins/SimpleThread/Source/SimpleThread/Private/Coroutines/SimpleCoroutines.cpp
@@ -0,0 +1,52 @@
+﻿#include "Coroutines/SimpleCoroutines.h"
+
+ICoroutinesObject::ICoroutinesObject()
+	: bAwaken(false)
+{
+
+}
+
+TArray<TSharedPtr<ICoroutinesObject>> ICoroutinesObject::mArray;
+
+void FCoroutinesObject::Update(FCoroutinesRequest& CoroutinesRequest)
+{
+	if (mTotalTime != INDEX_NONE) {
+		mRunningTime += CoroutinesRequest.IntervalTime;
+		if (mRunningTime >= mTotalTime) {
+			mSimpleDelegate.ExecuteIfBound();
+			CoroutinesRequest.bCompleteRequest = true;// 表明协程请求处理完了.
+		}
+	}
+	else {// Else则表明只有一个代理.
+		if (bAwaken == true) {
+			mSimpleDelegate.ExecuteIfBound();// 唤醒(执行)任务.
+			CoroutinesRequest.bCompleteRequest = true;// 表明协程请求处理完了.
+		}
+	}
+}
+
+FCoroutinesRequest::FCoroutinesRequest(float InIntervalTime)
+	: bCompleteRequest(false)
+	, IntervalTime(InIntervalTime)
+{
+
+}
+
+FCoroutinesObject::FCoroutinesObject(const FSimpleDelegate& InSimpleDelegate)
+	: ICoroutinesObject()// 调接口构造器.
+	, mSimpleDelegate(InSimpleDelegate)
+	, mTotalTime(INDEX_NONE)// INDEX_NONE为-1
+	, mRunningTime(0.0f)
+{
+	mArray.Add(this->AsShared());
+}
+
+FCoroutinesObject::FCoroutinesObject(float InTotalTime, const FSimpleDelegate& InSimpleDelegate)
+	: ICoroutinesObject()// 调接口构造器.
+	, mSimpleDelegate(InSimpleDelegate)
+	, mTotalTime(InTotalTime)
+	, mRunningTime(0.0f)
+{
+	mArray.Add(this->AsShared());
+
+}
---
+++ Plugins/SimpleThread/Source/SimpleThread/Public/Coroutines/SimpleCoroutines.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "Core/SimpleThreadType.h"
+
+/// 协程请求.
+struct FCoroutinesRequest
+{
+public:
+	FCoroutinesRequest(float InIntervalTime);
+
+	bool bCompleteRequest;
+	float IntervalTime;// 类似于deltaTime.
+};
+
+/// 协程接口.
+class ICoroutinesObject : public TSharedFromThis<ICoroutinesObject>
+{
+	friend class ICoroutinesContainer;
+
+public:
+	ICoroutinesObject();
+	virtual ~ICoroutinesObject() {};
+	//
+	bool operator==(const ICoroutinesObject& SimpleThreadHandle)
+	{
+		return this->Handle == SimpleThreadHandle.Handle;
+	};
+
+	//
+	FORCEINLINE void Awaken() { bAwaken = true; }
+protected:
+	virtual void Update(FCoroutinesRequest& CoroutinesRequest) = 0;
+
+protected:
+	static TArray<TSharedPtr<ICoroutinesObject>> mArray;
+	uint8 bAwaken : 1;// 用以唤醒当前的协程.
+	FSimpleThreadHandle Handle;
+};
+
+typedef TWeakPtr<ICoroutinesObject> FCoroutinesHandle;// 协程接口的弱句柄.
+
+/// 协程对象.
+class FCoroutinesObject : public ICoroutinesObject
+{
+public:
+	FCoroutinesObject(const FSimpleDelegate& InSimpleDelegate);
+	FCoroutinesObject(float InTotalTime, const FSimpleDelegate& InSimpleDelegate);
+
+protected:
+	virtual void Update(FCoroutinesRequest& CoroutinesRequest) final;
+
+protected:
+	FSimpleDelegate mSimpleDelegate;
+	const float mTotalTime;// 达到多长时间才执行函数,类似于delay.
+	float mRunningTime;
+
+};
